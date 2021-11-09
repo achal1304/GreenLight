3b59e984806f41134ba36495b1d595efd353a5a5
achal1304 <achal20000@gmail.com>
2021-11-09 14:07:03 +0530

    Performed basic CRUD operations

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,6 +14,9 @@ func (app *application) routes() http.Handler { // Initialize a new httprouter r
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler) // Return the httprouter instance.
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	return app.enableCORS(router)
 }
--- internal/data/models.go
+++ internal/data/models.go
@@ -9,6 +9,19 @@ var (
 	ErrRecordNotFound = errors.New("record not found")
 )
 
-type Models struct{ Movies MovieModel }
+type Models struct {
+	Movies interface {
+		Insert(movie *Movie) error
+		Get(id int64) (*Movie, error)
+		Update(movie *Movie) error
+		Delete(id int64) error
+	}
+}
 
-func NewModels(db *sql.DB) Models { return Models{Movies: MovieModel{DB: db}} }
+func NewModels(db *sql.DB) Models {
+	return Models{Movies: MovieModel{DB: db}}
+}
+
+func NewMockModels() Models {
+	return Models{Movies: MockMovieModel{}}
+}
