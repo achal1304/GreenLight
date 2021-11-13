2af7f43eedbb52de1df499df48ae7e88cb633846
achal1304 <achal20000@gmail.com>
2021-11-13 11:52:59 +0530

    User Activation : generating activation token and verifying

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,6 +22,7 @@ func (app *application) routes() http.Handler { // Initialize a new httprouter r
 
 	// router.HandlerFunc(http.MethodGet, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
